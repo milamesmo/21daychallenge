388f70c9fb7d20a1bcf09c55867a9d9e02474bb9
milamesmo <camilaabegg.n@gmail.com>
2025-07-02 21:42:20 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
+
